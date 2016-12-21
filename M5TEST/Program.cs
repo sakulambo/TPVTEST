010a8e8c8d8c2e38de7eecc4947a73e0ae105d87
sakulambo <sakulambo@KEVIN>
2016-12-21 20:51:32 +0100

    Add project files.

---
+++ M5TEST/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M5TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
